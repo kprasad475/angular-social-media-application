bf407b7c3fb19fe069cb98765136cd54d6356b7f
Binshabs <binshabs567@gmail.com>
2024-08-02 12:37:13 +0530

    components-generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostCreateComponent } from './post-create/post-create.component';
+import { PostUpdateComponent } from './post-update/post-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostCreateComponent,
+    PostUpdateComponent
   ],
   imports: [
     BrowserModule,
